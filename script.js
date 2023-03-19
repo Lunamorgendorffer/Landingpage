239231985fdefc172e6ff18552a4403d4320c463
Lunamorgendorffer <nabdou68@gmail.com>
2023-03-19 16:11:34 +0100

    ajout color picker

--- script.js
+++ script.js
@@ -2,3 +2,51 @@ function scrollToTop(){
     window.scrollTo(0,0);
 
 }
+
+// CHANGE COLOR THEME
+
+function changeStyle(){
+  var element = document.getElementById("home");
+  element.style.backgroundColor = "#00FF00";
+}
+
+function changeColor(color) { 
+  var innerDiv = document.querySelector("#home");
+  var titles = document.querySelectorAll("h2");
+  var cards = document.querySelectorAll(".card");
+  var rounds = document.querySelectorAll(".sale");
+  var signs = document.querySelectorAll(".symbol");
+  var numbers = document.querySelectorAll(".amount");
+  var buttons = document.querySelectorAll(".priceButton");
+  
+  innerDiv.style.background = color; 
+
+  titles.forEach(title => {
+    title.style.color = color;
+  });
+
+  cards.forEach(card => {
+    card.style.background = color;
+    card.style.color = "white";
+  });
+
+  rounds.forEach( round=> {
+    round.style.background = color;
+    round.style.color = "white";
+    round.style.border= color;
+  });
+  
+  signs.forEach(sign => {
+    sign.style.color = color;
+  });
+
+  numbers.forEach(number => {
+    number.style.color = color;
+  });
+
+  buttons.forEach( button=> {
+    button.style.background = color;
+    button.style.color = "white";
+    button.style.border= color;
+  }); 
+}
